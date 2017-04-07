77826376c8b87791abcfd1321ba94855068e4084
wingtonbrito <wingtonbrito@gmail.com>
2017-04-07 09:25:58 -0600

    Need to debug the comment part

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react'
 
-
-
 export default class Comments extends Component{
+  renderComment(comment, i){
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  },
+  handleSubmit(e){
+    e.preventDefault();
+    const { postId } =  this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
+  }
   render(){
     return (
       <div className="comment">
-        coments!!!!!
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form" onSubmit={ this.handleSubmit} >
+          <iput type="text" ref="author" placeholder="author"/>
+          <iput type="text" ref="comment" placeholder="comment"/>
+          <iput type="submit" hidden />
+        </form>
       </div>
     )
   }
